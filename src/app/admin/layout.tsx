d82ef8238e22717d8c7aab684096081d4c5fa992
Zefulab <mraka607@gmail.com>
2024-12-19 20:05:48 +0700

    sign

--- src/app/admin/layout.tsx
+++ src/app/admin/layout.tsx
@@ -1,20 +1,19 @@
 "use client";
+
 import Login from "@/components/admin-panel/Login";
 import { useAppSelector } from "@/redux/hook";
 import { useSession } from "next-auth/react";
 import React from "react";
 
-const layout = () => {
+const Layout = () => {
+  const isLoading = useAppSelector((store) => store.LoadingReducer);
+  const { data: session } = useSession();
 
-    const isloading = useAppSelector((store) => store.LoadingReducer)
-    const { data : session} = useSession()
-
-    if(!session?.user){
-      return<Login />;
-    }
+  if (!session?.user) {
+    return <Login />;
+  }
 
   return <div>layout</div>;
-  
 };
 
-export default layout;
+export default Layout;
--- src/components/admin-panel/Login.tsx
+++ src/components/admin-panel/Login.tsx
@@ -1,10 +1,14 @@
-import { FcGoogle } from "react-icons/fc";
+import { signIn } from "next-auth/react"
+import { FcGoogle } from "react-icons/fc"
 
 const Login = () => {
-  return  <div className="bg-black main-h-screen grid place-items-center">
-      <button className="bg-white px-8 py-4 flex gap-2 items-center">
-        <FcGoogle size={30} /> Sign In with Google</button>
+  return (
+    <div className="bg-black min-h-screen grid place-items-center">
+      <button className="bg-white px-8 py-4 flex gap-2 items-center" onClick={() => signIn ("google")}> 
+        <FcGoogle size={30} /> Sign With Google
+        </button>
     </div>
+  );
 };
 
 export default Login;
